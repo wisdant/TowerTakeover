9f7f0efa6130a4b7205c4f84f29ef36f1f7c0e6d
Richard Liu <wisdant@users.noreply.github.com>
2019-09-15 14:54:08 -0700

    Updated port number per the new build

--- opcontrol.cpp
+++ opcontrol.cpp
@@ -1,36 +1,74 @@
 #include "main.h"
 
 /**
- * This is basic robot code that allows your robot to move and manipulate objects using a remote controller.
- * 
- * While this code does allow the robot to fully function, it can be improved and expanded in numerous ways.
- * If you desire, you can talk to a volunteer about ways to improve this code to better prepare it for competitions ahead.
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
  */
+/*void opcontrol() {
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Motor left_mtr(1);
+	pros::Motor right_mtr(2);
+	while (true) {
+		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
+		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
+		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		int left = master.get_analog(ANALOG_LEFT_Y);
+		int right = master.get_analog(ANALOG_RIGHT_Y);
 
+		left_mtr = left;
+		right_mtr = right;
+		pros::delay(20);
+	}
+}*/
 const double DRIVE_SPEED = 0.75; // Changes the maximum drive speed (range between 0 and 1)
 const int ARCADE = true; // Controls whether the drive is tank style (false) or arcade style (true)
 const int DRIVE_THRESHOLD  = 12; // Controls the minimum power that can be assigned to drive motors
-const int LEFT_DRIVE_PORT = 1;
-const int RIGHT_DRIVE_PORT = 10;
-const int LIFT_PORT = 8;
-const int CLAW_PORT = 3;
+
+const int RIGHT_FRONT_PORT = 1;
+const int RIGHT_REAR_PORT = 4;
+const int RIGHT_ARM_PORT = 5;
+
+const int LIFT_PORT = 2;
+const int HINGE_PORT = 3;
+
+const int LEFT_FRONT_PORT = 10;
+const int LEFT_REAR_PORT = 9;
+const int LEFT_ARM_PORT = 8;
 
 // Define the main controller of the robot.
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 // Define the robot's motors with their respective ports.
 // Passing true for the second parameter reverses the motor.
-pros::Motor left_drive (LEFT_DRIVE_PORT, false);
-pros::Motor right_drive (RIGHT_DRIVE_PORT, true);
+pros::Motor left_drive (LEFT_FRONT_PORT, false);
+pros::Motor left_rear_drive (LEFT_REAR_PORT, false);
+
+pros::Motor right_drive (RIGHT_FRONT_PORT, true);
+pros::Motor right_rear_drive (RIGHT_REAR_PORT, true);
+
+pros::Motor left_arm (LEFT_ARM_PORT, false);
+pros::Motor right_arm (RIGHT_ARM_PORT, true);
+
 pros::Motor lift (LIFT_PORT, false);
-pros::Motor claw (CLAW_PORT, false);
+pros::Motor hinge (HINGE_PORT, false);
 
 /**
  * Assign specified powers for the right and left sides of the drivetrain to the left and right motors, respectively.
  */
 void move_drive(int right, int left) {
 	right_drive.move(right);
+	right_rear_drive.move(right);
 	left_drive.move(left);
+	left_rear_drive.move(left);
 }
 
 /**
@@ -43,8 +81,8 @@ void move_lift(int power) {
 /**
  * Assign a specified power to the claw motor.
  */
-void move_claw(int power) {
-	claw.move(power);
+void move_hinge(int power) {
+	hinge.move(power);
 }
 
 /**
@@ -141,20 +179,20 @@ void opcontrol() {
 		 * This code controls the claw on the robot
 		 */
 		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) == 1) {
-			move_claw(127); // Close the claw
+			move_hinge(127); // Close the claw
 			while (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) == 1) {
 				pros::delay(10);
 			}
-			move_claw(25); // Assign a little power and allow the motor to hold it's position.
+			move_hinge(25); // Assign a little power and allow the motor to hold it's position.
 		}
 		else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2) == 1) {
-			move_claw(-127); // Open the claw
+			move_hinge(-127); // Open the claw
 			while (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2) == 1) {
 				pros::delay(10);
 			}
-			move_claw(-10); // Assign a little power and allow the motor to hold it's position.
+			move_hinge(-10); // Assign a little power and allow the motor to hold it's position.
 		}
-		
+
 
 		// IMPORTANT: DO NOT REMOVE
 		// This delay allows all other robot related functionality to be run between iterations of this while loop.
